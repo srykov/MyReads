5a55104d140cd0421a356b39c7b7ed136bbffdbd
Sara Rykov <sara.rykov@gmail.com>
2018-04-03 10:47:17 -0400

    feature: Add Book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Book from './Book'
 
 class BookShelf extends Component {
 
@@ -10,26 +11,7 @@ class BookShelf extends Component {
 	          <div className="bookshelf-books">
 	            <ol className="books-grid">
 					{this.props.books.map((book) => (
-
-	                  <li key={book.id}>
-						<div className="book">
-	                      <div className="book-top">
-	                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
-	                        <div className="book-shelf-changer">
-	                          <select>
-	                            <option value="none" disabled>Move to...</option>
-	                            <option value="currentlyReading">Currently Reading</option>
-	                            <option value="wantToRead">Want to Read</option>
-	                            <option value="read">Read</option>
-	                            <option value="none">None</option>
-	                          </select>
-	                        </div>
-	                      </div>
-	                      <div className="book-title">{book.title}</div>
-	                      <div className="book-authors">{book.authors}</div>
-	                    </div>
-	                  </li>
-
+	                  <Book book={book}/>
 					))}
 				</ol>
 			  </div>
